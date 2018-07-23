7f91ce57a915e7f234f294f3ead78b8c92805d25
yelgogogo <yelgogogo@hotmail.com>
2018-07-23 20:01:21 +0800

    时间获取最近一周

--- analyzer.js
+++ analyzer.js
@@ -1,5 +1,6 @@
 const db = require('diskdb');
 const yaml = require('js-yaml');
+const moment = require('moment');
 
 const dayHours = []
 for(let i=23;i>=0;i--) {
@@ -12,15 +13,25 @@ for(let i=23;i>=0;i--) {
   dayHours.push( hour )
 }
 db.connect('db', ['logs']);
-let date = '2018-07-23'
-let dayArray = db.logs.find({date})
+let timeArray = []
+for(let i=6;i>=0;i--) {
+  const element = moment().subtract(i,'days').format('YYYY-MM-DD')
+  if (element > '2018-07-20') {
+    timeArray.push(element)
+  }
+}
+// let date = '2018-07-23'
 
-const hourStatistics =  dayHours.map(h => {
-  let records = dayArray.filter(d => d.hour === h)
-  let value = records.length
-  let hour = h
-  return {date, hour, value}
-})
 db.connect('db', ['hourStatistics']);
-db.hourStatistics.remove({date}, true);
-db.hourStatistics.save(hourStatistics)
+
+timeArray.forEach(date => {
+  let dayArray = db.logs.find({date})
+  const hourStatistics =  dayHours.map(h => {
+    let records = dayArray.filter(d => d.hour === h)
+    let value = records.length
+    let hour = h
+    return {date, hour, value}
+  })
+  db.hourStatistics.remove({date}, true);
+  db.hourStatistics.save(hourStatistics)
+})
