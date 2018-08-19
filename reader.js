555cecba30625bf1c0c660f71861afb881f3d2fc
yelgogogo <yelgogogo@hotmail.com>
2018-08-19 22:55:30 +0800

    增加 小時 日 月 統計

--- reader.js
+++ reader.js
@@ -3,104 +3,131 @@ const yaml = require('js-yaml');
 const {PROP, CATEGORY} = require('./utils/enum')
 const db = require('diskdb');
 
-const translator = (value) => {
-  const typeTxt = value.replace(/[0-9]/g, '')
-  const type = PROP[typeTxt]?PROP[typeTxt]:''
-  numReg = /[0-9]+/
-  let num =  numReg.exec(value) ? numReg.exec(value)[0] : ''
-  if (type==='FCR' && num==='') {
-    num = 10
-  }
-  let res = {}
-  res[type] = true
-  return Object.assign(res, {propArray:[{name: type, value: num}]})
-}
-
 const ownerArray = [
-  {owner:'大仁哥',url:'http://bbs.impk.cc/ShowTopic-8085318-124.php?type=dyn'},
-  {owner:'献血之披风',url:'http://bbs.impk.cc/ShowTopic-8164609-124.php?type=dyn'},
-  {owner:'iasias',url:'http://bbs.impk.cc/ShowTopic-8191583-124.php?type=dyn'},
-  {owner:'zkkkk',url:'http://bbs.impk.cc/ShowTopic-8186758-124.php?type=dyn'},
-  {owner:'parke--impk',url:'http://bbs.impk.cc/ShowTopic-8193528-124.php?type=dyn'}
+  {key:'201808',url:''},
 ]
-// const ownerArray = [{owner:'sample', url:'http://bbs.impk.cc/ShowTopic-8186758-124.php?type=dyn'}]
+
+db.connect('db', ['hourRecords']);
+db.connect('db', ['dayRecords']);
+db.connect('db', ['monthRecords']);
+
+// const ownerArray = [{month:'sample', url:'http://bbs.impk.cc/ShowTopic-8186758-124.php?type=dyn'}]
 const readFile = (ownerObj) => {
-  let {owner, url} = ownerObj
-  fs.readFile(`./data/${owner}`, 'utf8', (err, data) => {
+  let {key, url} = ownerObj
+  fs.readFile(`./data/${key}`, 'utf8', (err, data) => {
     // console.log(data);  
-    const file = owner.split('_')
-    const arr = data.toUpperCase().split('\n')
-    let category = ''
-    db.connect('db', ['goods']);
-    db.goods.remove({owner}, true);
-    arr.forEach(goods => {
-      goods = goods.trim()
-      if (!goods) {
-        category = ''
-      }
-      // console.log(goods)
-      if (CATEGORY[goods]) {
-        category = CATEGORY[goods]
-      } else {      
-        if (category) {
-          db.goods.save(setGoods(owner, url, category, goods));
+    const arr = data.split('\n')
+    
+    db.hourRecords.remove({key}, true);
+    db.dayRecords.remove({key}, true);
+    db.monthRecords.remove({key}, true);
+
+
+    let lastHour = ''
+    let lastId = ''
+    let arrHour = []
+    let tempHourData = []
+    
+    arr.forEach(hourRecords => {
+      
+      const row = hourRecords.split(',')
+      if(!row[0]) {
+        arrHour = arrHour.concat(tempHourData)
+        return
+      } 
+      let name = row[0]
+      let id = row[1]
+      let fullTime = row[2].substr(0,19)
+      let year = fullTime.substr(0,4)
+      let month = fullTime.substr(5,2)
+      let day = fullTime.substr(8,2)
+      let date = fullTime.substr(0,10)
+      let hour = fullTime.substr(11,2)
+      let thisHour = fullTime.substr(0,13)
+      if(thisHour === lastHour) {
+        let finder = tempHourData.findIndex(t => t.id === id)
+        if(finder === -1) {
+          let num = 1
+          tempHourData.push({key, id, date, year, month, day, hour, num})
+        } else {
+          tempHourData[finder].num += 1
         }
+        
+      } else {
+        lastHour = thisHour;
+        if(tempHourData.length !== 0) {
+          arrHour = arrHour.concat(tempHourData)
+        }
+        tempHourData = []
+        let num = 1
+        tempHourData.push({key, id, date, year, month, day, hour, num})
       }
     })
+
+    db.hourRecords.save(arrHour)
   
-  });
+    // get day data
+    let lastDay = ''
+    let tempDayData = []
+    let arrDay = []
+
+    arrHour.forEach(hourData => {
+      let thisDay = hourData.date
+      if (thisDay === lastDay) {
+        let finder = tempDayData.findIndex(t => t.id === hourData.id)
+        if(finder === -1) {
+          const {key, id, date, year, month, day, num} = hourData
+          tempDayData.push({key, id, date, year, month, day, num})
+        } else {
+          tempDayData[finder].num += hourData.num
+        }
+      } else {
+        lastDay = thisDay;
+        if(tempDayData.length !== 0) {
+          arrDay = arrDay.concat(tempDayData)
+        }
+        tempDayData = []
+        const {key, id, date, year, month, day, num} = hourData
+        tempDayData.push({key, id, date, year, month, day, num})
+      }
+    })    
+    if(tempDayData.length !== 0) {
+      arrDay = arrDay.concat(tempDayData)
+    }
+    db.dayRecords.save(arrDay)
+
+    // get month data
+    let lastMonth = ''
+    let tempMonthData = []
+    let arrMonth = []
+
+    arrDay.forEach(dayData => {
+      let thisMonth = dayData.year + ' ' + dayData.month
+      if (thisMonth === lastMonth) {
+        let finder = tempMonthData.findIndex(t => t.id === dayData.id)
+        if(finder === -1) {
+          const {key, id, year, month, num} = dayData
+          tempMonthData.push({key, id, year, month, num})
+        } else {
+          tempMonthData[finder].num += dayData.num
+        }
+      } else {
+        lastMonth = thisMonth;
+        if(tempMonthData.length !== 0) {
+          arrMonth = arrMonth.concat(tempMonthData)
+        }
+        tempMonthData = []
+        const {key, id, year, month, num} = dayData
+        tempMonthData.push({key, id, year, month, num})
+      }
+    })    
+    if(tempMonthData.length !== 0) {
+      arrMonth = arrMonth.concat(tempMonthData)
+    }
+    db.monthRecords.save(arrMonth)
+  })
 }
 
 ownerArray.forEach(o => {
   readFile(o);
 })
-
-const setGoods = (owner, url, category,goodsStr ) => {
-  const text = goodsStr.trim()
-  let priceReg = /\=\s*[0-9]+\.{0,1}[0-9]{0,2}/
-  let priceTest = priceReg.exec(text)
-  let price = 0
-  let priceValue = 0
-  let comment = ''
-  let propTxt = ''
-  propTxt = text
-  if(priceTest) {
-    price = parseFloat(priceTest[0].replace(/\=/, '').trim())
-    comment = text.substr(priceTest.index).replace(priceReg, '').trim()
-    if(comment.indexOf('IST') !== -1) {
-      priceUnit = 'IST'
-      priceValue = price * 40
-    } else if (comment.indexOf('PG') !== -1) {
-      priceUnit = 'PG'
-      priceValue = price
-    } else if (comment.indexOf('#') !== -1) {
-      if (price < 24) {
-        priceUnit = 'PG'
-        price = Math.pow(2,(price - 20)) * 2.5
-        priceValue = price
-      } else if (price > 23){
-        priceUnit = 'IST'
-        price = Math.pow(2,(price - 24))
-        priceValue = price * 40
-      }
-    } else {
-      priceUnit = 'IST'
-      priceValue = price * 40
-    }
-    propTxt = text.substr(0, priceTest.index)
-  }
-  // console.log(price, priceUnit)
-  let propArr = propTxt.trim().replace(/\s\s*/g,' ').split(' ')
-  let prop = {}
-  let propArray = []
-  propArr.forEach(p => {
-    const allProp = translator(p)
-    propArray = propArray.concat(allProp.propArray)
-    delete allProp.propArray
-    prop = Object.assign(prop, allProp)
-  })
-  let goods = Object.assign({text, owner, url, category, price, priceUnit, priceValue, comment}, prop, {propArray: propArray})
-  return goods
-  
-}
-
